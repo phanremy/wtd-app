3982b4e3595605478e71e16778a7bbd9e4e473a2
remy phan <phan.remy@gmail.com>
2022-06-27 09:30:14 +0200

    filter front

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "posts#index"
   resources :posts
+  namespace :posts do
+    resource :filters, only: :create
+  end
 end
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -6,10 +6,19 @@ module.exports = {
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}'
   ],
+  variants: {
+    borderColor: ['responsive', 'hover', 'focus', 'focus-within'],
+  },
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      transformOrigin: {
+        "0": "0%",
+      },
+      zIndex: {
+        "-1": "-1",
       },
     },
   },
